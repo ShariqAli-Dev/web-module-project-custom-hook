61e578dce7f5430ae58b8873bd0f0bdd18334b9a
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-08-10 17:31:06 -0400

    completed mvp

--- src/hooks/useDarkModed.js
+++
@@ -1,7 +0,0 @@
-import useLocalStorage from "./useLocalStorage";
-
-export default function useDarkMode() {
-  const [useDarkMode, setUseDarkMode] = useLocalStorage("darkModeEnabled");
-
-  return [useDarkMode, setUseDarkMode];
-}
